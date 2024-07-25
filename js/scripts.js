5cc1d5babc546a7391b7dd92b4490a42c3b888af
unknown <ricardodonado1105@gmail.com>
2024-07-24 21:13:20 -0500

    imported elements as consts in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
-console.log("hello world!")
+const balanceAmountElement = document.getElementsByClassName("balanceAmount")[0]
+const progressBarElements = [...document.getElementsByClassName("progressBar")]
+const monthTotalElement = document.getElementsByClassName("monthTotal")
+
