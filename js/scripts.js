77ed8a427802bfd1e456b8c59c57c2dc4d56d806
unknown <ricardodonado1105@gmail.com>
2024-07-24 22:57:45 -0500

    bar values randomized, value assigned + highest status

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,19 @@ const balanceAmountElement = document.getElementsByClassName("balanceAmount")[0]
 const progressBarElements = [...document.getElementsByClassName("progressBar")]
 const monthTotalElement = document.getElementsByClassName("monthTotal")
 
+let randomData = Array(progressBarElements.length).fill(0)
+
+for(let i=0; i < progressBarElements.length; i++){
+    randomData[i] = Math.random()*55
+    progressBarElements[i].style.height = `${randomData[i]*1.5}%`
+}
+
+const highestValue = Math.max(...randomData)
+
+for(let i=0; i < progressBarElements.length; i++){
+    if(progressBarElements[i].style.height === `${(highestValue*1.5).toFixed(4)}%`){
+        progressBarElements[i].classList.add("highest")
+    }else{
+        progressBarElements[i].classList.remove("highest")
+    }
+}
